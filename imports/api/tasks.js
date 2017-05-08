761bfd1997ed562c4d80f58ccd79aa7c43db17cb
ndunhourowens <ndunhour@hotmail.com>
2017-05-07 21:48:39 -1000

    searchfield returns results

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -12,30 +12,40 @@ if (Meteor.isServer) {
 }
 
 Meteor.methods({
-  'tasks.insert'(text) {
-    check(text, String);
+    'tasks.insert'(text) {
+        check(text, String);
 
-    // Make sure the user is logged in before inserting a task
-    if (! Meteor.userId()) {
-      throw new Meteor.Error('not-authorized');
+        // Make sure the user is logged in before inserting a task
+        if (! Meteor.userId()) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username,
+            });
+    },
+    'tasks.remove'(taskId) {
+        check(taskId, String);
+
+        Tasks.remove(taskId);
+    },
+    'tasks.setChecked'(taskId, setChecked) {
+        check(taskId, String);
+        check(setChecked, Boolean);
+
+        Tasks.update(taskId, { $set: { checked: setChecked } });
+    },
+    'findSong'(songToFind, songs) {
+        const filterSearch = songs.filter(function(el){
+            const regex = new RegExp(songToFind, 'gi');
+            return el.Title.match(regex) || el.Artist.match(regex);
+        });
+
+        return filterSearch;
+
     }
 
-    Tasks.insert({
-      text,
-      createdAt: new Date(),
-      owner: Meteor.userId(),
-      username: Meteor.user().username,
-    });
-  },
-  'tasks.remove'(taskId) {
-    check(taskId, String);
-
-    Tasks.remove(taskId);
-  },
-  'tasks.setChecked'(taskId, setChecked) {
-    check(taskId, String);
-    check(setChecked, Boolean);
-
-    Tasks.update(taskId, { $set: { checked: setChecked } });
-  },
 });
--- imports/ui/searchfield.js
+++ imports/ui/searchfield.js
@@ -3,14 +3,26 @@ import { Template } from 'meteor/templating';
 
 import './searchfield.html';
 
+Template.searchfield.onCreated( function(){
+
+})
+
 Template.searchfield.helpers({
   isOwner() {
     return this.owner === Meteor.userId();
   },
+
 });
 
 Template.searchfield.events({
-  'keyup .searchfield'(event){
-    console.log(event.key);
-  }
+  'submit .searchfield': function(event){
+    event.preventDefault();
+    const searchValue = event.target.text.value;
+
+    Meteor.call('findSong', searchValue, songs, function(err, result){
+        console.log(result);
+    })
+
+  },
+
 });
