f2d3bab044d45c47984bb3e97ca8630a8a42caa0
ndunhourowens <ndunhour@hotmail.com>
2017-05-23 00:03:04 -1000

    complete color scheme for logout

--- client/logout.js
+++ client/logout.js
@@ -11,14 +11,14 @@ Template.logout.helpers({
 
 Template.logout.events({
     'click .cancelLogout': function(e){
-        if(Meteor.users.find().count() > 0){
-            Router.go('/adminDash/' + Meteor.userId());
-        }else{
-            Router.go('/userDash');
-        }
+        // if(Meteor.users.find().count() > 0){
+        //     Router.go('/adminDash/' + Meteor.userId());
+        // }else{
+        //     Router.go('/userDash');
+        // }
     },
     'click .yesLogout': function(e){
-        Meteor.logout();
-        Router.go('/');
+        // Meteor.logout();
+        // Router.go('/');
     }
 });
